b2c55867f3ba4340f1fa42a57c3f815ee0204ea0
Jakob Odersky <jodersky@gmail.com>
2013-04-16 23:44:29 +0200

    polish rxtx test

--- scala/rxtx/src/test/scala/com/github/jodersky/ace/rxtx/test/Main.scala
+++ scala/rxtx/src/test/scala/com/github/jodersky/ace/rxtx/test/Main.scala
@@ -10,7 +10,7 @@ import scala.concurrent.ExecutionContext.Implicits.global
 object Main {
   
   def main(args: Array[String]): Unit = {
-    val serial = new Serial("/dev/ttyACM0")
+    val serial = new Serial("/dev/ttyACM0", 9600)
     val framer = new Framer
     val arq = new Arq(200)
     val app = new SimpleActionLayer((s: Seq[Int]) => println(s))
@@ -18,8 +18,10 @@ object Main {
     serial connect framer connect arq connect app
     serial.begin()
     
+    print(">")
     while (true) {
-      app.send(Console.readLine.getBytes().map(_.toInt)).map(sent => Console.println("> " + sent))
+      val bytes = readLine.getBytes().map(_.toInt)
+      app.send(bytes).map(sent => "[sent] " + sent).recover{case ex => "[failed] " + ex}.map{s => println(s); print(">")}
     }
     
   }
